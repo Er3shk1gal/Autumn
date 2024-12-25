4c58c3e608d3390d946c644daf09d7f3b11ab714
ereshkigal <roman.khlynin@bk.ru>
2024-12-25 17:31:10 +0300

    preparing JSON message handler

--- Kafka/Autumn.Kafka/MessageHandlers/JsonMessageHandler.cs
+++ Kafka/Autumn.Kafka/MessageHandlers/JsonMessageHandler.cs
@@ -13,11 +13,35 @@ namespace Autumn.Kafka.MessageHandlers
         private readonly IProducer<string,string> _producer;
         private readonly IConsumer<string,string> _consumer;
         private readonly TopicConfig _requestTopicConfig;
-        public JsonMessageHandler(IProducer<string, string> producer, IConsumer<string, string> consumer, TopicConfig requestTopicConfig)
+        private readonly MessageHandlerConfig _messageHandlerConfig;
+        public JsonMessageHandler(IProducer<string, string> producer, IConsumer<string, string> consumer, TopicConfig requestTopicConfig, MessageHandlerConfig messageHandlerConfig)
         {
             _producer = producer;
             _consumer = consumer;
             _requestTopicConfig = requestTopicConfig;
+            _messageHandlerConfig = messageHandlerConfig;
+        }
+        public async Task Consume()
+        {
+            try
+            {
+                _consumer.Assign(GetPartitions());
+                while (true)
+                {
+                    var message = _consumer.Consume();
+                    var messageObject = JObject.Parse(message.Value);
+                    Console.WriteLine($"Message received: {messageObject}");
+            
+            }
+        }
+        private List<TopicPartition> GetPartitions()
+        {
+            var partitions = new List<TopicPartition>();
+            for (int partition = 0; partition < _requestTopicConfig.PartitionsCount; partition++)
+            {
+                partitions.Add(new TopicPartition(_requestTopicConfig.TopicName, partition));
+            }
+            return partitions;
         }
         public async Task HandleMessage()
         {
